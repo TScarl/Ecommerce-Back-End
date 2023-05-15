621ecd51177750266e0ae75ef23846719c3052da
TScar <tscarlett@live.com.au>
2023-05-15 11:16:58 +1000

    initial commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const updatedCategory = await Category.findByPk(req.params.id);
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+await Category.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+    res.status(200).json({message: 'Category deleted'});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const updatedTag = await Tag.findByPk(req.params.id);
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+try {
+  await Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json({message: 'Tag destroyed'});
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+module.exports = router;
